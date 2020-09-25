429c9b3e2e3f8525a77fc4b87f023c9e9d6fa164
k1te <kmiller343@gmail.com>
2020-09-25 18:14:56 -0500

    added else if statements for all user choices

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var howLong;
 var confirmLower;
 var confirmUpper;
-var confrimNum;
+var confirmNum;
 var confirmSpecial;
 
 // Password Values
@@ -16,6 +16,21 @@ number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 //Alphabetical characters
 alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+//For Uppercase conversions
+uAlpha = [];
+
+//Convert to Uppercase
+var toUpper = function(x) {
+  return x.toUpperCase();
+}
+
+//Uppercase conversion variable
+alpha2 = alpha.map(toUpper);
+
+//User Choices
+var userChoices;
+
+
 
 function generatePassword(){
   howLong=parseInt(prompt("Please specify your password length (8-128 Characters)."));
@@ -25,14 +40,55 @@ function generatePassword(){
     alert("Must be between 8 and 128 characters.");
   } else {
     confirmLower = confirm("Use lowercase letters?");
-    confirmUpper = confirm("Use upper case letters?");
+    confirmUpper = confirm("Use uppercase letters?");
     confirmNum = confirm("Use numbers?");
     confirmSpecial = confirm("Use special characters?");
   };
+
+
+  
   if (!confirmLower && !confirmUpper && !confirmNum && !confirmSpecial) {
     alert("You must choose at least one criteria!");
+  } else if (confirmLower && confirmUpper &&confirmNum && confirmSpecial){ // All True Confirms
+    userChoices = alpha.concat(alpha2, number, character);
+  } 
+  
+  else if (confirmLower && confirmUpper && confirmNum) { // 3 True Confirms
+    userChoices = alpha.concat(alpha2, number);
+  } else if (confirmLower && confirmUpper && confirmSpecial) {
+    userChoices = alpha.concat(alpha2, character);
+  } else if (confirmLower && confirmNum && confirmSpecial) {
+    userChoices = alpha.concat(number, character);
+  } else if (confirmUpper && confirmNum && confirmSpecial) {
+    userChoices = alpha2.concat(number, character);
+  } 
+  
+  else if (confirmSpecial && confirmLower) { // 2 True Confirms
+    userChoices = character.concat(alpha);
+  } else if (confirmSpecial && confirmUpper) {
+    userChoices = character.concat(alpha2);
+  } else if (confirmSpecial && confirmNum) {
+    userChoices = character.concat(number);
+  } else if (confirmLower && confirmUpper) {
+    userChoices = alpha.concat(alpha2);
+  } else if (confirmLower && confirmNum) {
+    userChoices = alpha.concat(nubmer);
+  } else if (confirmUpper && confirmNum) {
+    userChoices = alpha2.concat(number);
+  } 
+  
+  else if (confirmLower) { // 1 True Confirm
+    userChoices = alpha;
+  } else if (confirmUpper) {
+    userChoices = alpha2;
+  } else if (confirmNum) {
+    userChoices = number;
+  } else if (confirmSpecial) {
+    userChoices = character;
   }
+  
 };
+
 
 
   
